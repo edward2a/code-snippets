5ab1a49645ca1753ad3a29cb94b9c35e3e2a7f84
Eduardo A. Paris Penas <edward2a@gmail.com>
2025-01-08 22:19:54 +0000

    python logging+threads sample

---
+++ python/logging_with_threads.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+import logging
+import time
+
+from threading import Thread
+
+format = '%(asctime)s.%(msecs)03d %(levelname)s: (%(threadName)s) %(message)s'
+date_format = '%Y-%m-%dT%H:%M:%SZ'
+logging.Formatter.converter = time.gmtime
+logging.basicConfig(level=logging.DEBUG, format=format, datefmt=date_format)
+log = logging.getLogger()
+
+class HelloWorld(Thread):
+    def run(self, **kwargs):
+        log.debug('Hello, World! I AM A THREAD!.')
+
+def hw():
+    log.warning('I am a function.')
+
+
+log.info('Test message.')
+hw()
+t = HelloWorld()
+t.start()
+t.join()
+
